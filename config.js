f90a632b55954a3c4ad6eea21d282e95c5029c13
warmanherry <warmanherry@gmail.com>
2026-07-20 23:44:57 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+window.SHISHOUYAN_CONFIG = {
+  // 部署 Google Apps Script 後，把 /exec 網址貼在下面。
+  scriptUrl: "https://script.google.com/macros/s/AKfycby4cbtiD3fV3wWeWKh2p4Hkh35U3Yd1IqXhqBnvUJp6EEEVBdvFyE0D9x5BnB79kmhU/exec",
+
+  // 必須和 Code.gs 裡的 ACCESS_TOKEN 完全相同。
+  accessToken: "305388Jq"
+};
