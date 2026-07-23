ba759e41daf2b4fac4bab223ba2aa7df5e5820ba
warmanherry <warmanherry@gmail.com>
2026-07-23 19:57:56 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='shishouyan-google-sheets-v1-1-3-list-navigation';
+const CACHE='shishouyan-google-sheets-v1-2-beta-auth-r2';
 const FILES=['./','./index.html','./config.js','./manifest.json'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)));
